28334919d4a790303928ac50ee455083b3604329
E-Holt <emma.d.holt@gmail.com>
2023-01-12 12:31:12 +1000

    Populate using virtual(product) in cart

--- src/controllers/carts/cartRoutes.js
+++ src/controllers/carts/cartRoutes.js
@@ -4,29 +4,46 @@ const { getCarts, getCartById, getCartByUserId } = require("./cartControllers")
 
 const cartRouter = express.Router()
 
-cartRouter.get("/", (request, response) => {
-    const carts = getCarts()
-    response.json(carts)
+cartRouter.get("/", async (request, response) => {
+  const carts = await getCarts()
+  response.json(carts)
 })
 
-cartRouter.get("/:cartId", (request, response) => {
-    const cart = getCartById(request.params.cartId)
-    if (!cart) {
-        response.status(404).json({
-            data: "cart doesn't exist",
-        })
-    }
-    response.json(cart)
+cartRouter.get("/:cartId", async (request, response) => {
+  const cart = await getCartById(request.params.cartId)
+  if (!cart) {
+      response.status(404).json({
+          data: "cart doesn't exist",
+      })
+  }
+  response.json(cart)
 })
 
-cartRouter.get("/user/:userId", (request, response) => {
-    const cart = getCartByUserId(request.params.userId)
-    if (!cart) {
-        response.status(404).json({
-            data: "cart doesn't exist",
-        })
-    }
-    response.json(cart)
+
+cartRouter.get("/:cartId", async (request, response) => {
+  const cart = await getCartById(request.params.cartId)
+  if (!cart) {
+      response.status(404).json({
+          data: "cart doesn't exist",
+      })
+  }
+  response.json(cart)
+})
+
+// /carts/user/userId?getProductInfo=true
+cartRouter.get("/user/:userId", async (request, response) => {
+let cart
+if (request.query.getProductInfo) {
+    cart = await getCartByUserIdWithProductInfo(request.params.userId)
+} else {
+    cart = await getCartByUserId(request.params.userId)
+}
+if (!cart) {
+    response.status(404).json({
+        data: "cart doesn't exist",
+    })
+}
+response.json(cart)
 })
 
 module.exports = cartRouter
