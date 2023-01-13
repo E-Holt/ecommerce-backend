2bc79964dee6371d88cffb41f204e71d01494a5d
E-Holt <emma.d.holt@gmail.com>
2023-01-13 10:04:06 +1000

    Delete functionality for product

--- src/controllers/products/productControllers.js
+++ src/controllers/products/productControllers.js
@@ -1,26 +1,5 @@
 const Product = require("/Users/admin/Desktop/Workspaces/Term-3/ecommerce-backend/src/models/products.js")
 
-const products = [
-  {
-    title: "Bag",
-    description: "Bag for all occasions",
-    price: 42,
-    stock: 10,
-  },
-  {
-    title: "Ring",
-    description: "Wedding Ring",
-    price: 4200,
-    stock: 5,
-  },
-  {
-    title: "Wallet",
-    description: "Wallet for all occasions",
-    price: 420,
-    stock: 15,
-  },
-]
-
 async function getProducts() {
   //get the products from the database
   const products = await Product.find()
@@ -41,6 +20,13 @@ async function createProduct(product) {
   //insert the product into the database and return that created product
   const newProduct = await Product.create(product)
   return newProduct
+}
+
+async function deleteProduct(productId) {
+  //deteleOne and deleteMany will delete but not return the records
+  //findByIdAndDelete and findOneAndDelete will detele and return the deleted record
+  const deletedProduct = await Product.findByIdAndDelete(productId)
+  return deletedProduct
 }
 
 module.exports = {
